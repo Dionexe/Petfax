de5aa08996b01d38b2b58c89451ab6d4529f062a
Dion Wells <dionwells@Dions-MacBook-Pro.local>
2024-05-24 10:51:39 -0700

    Fixed Animal Photos

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -30,7 +30,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/dionwells/Class/Python/PY-petfax-app/venv"
+set -gx VIRTUAL_ENV "/Users/dionwells/Class/Python/Petfax/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
